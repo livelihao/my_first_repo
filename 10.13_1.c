1d901f0dcb4e902c8ef34859ab7fcf801ca34b78
livelihao <livehao@163.com>
2014-10-13 07:54:35 +0800

    北大OJ

---
+++ 10.13_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define LEW 5
+#include <math.h>
+int main()
+{
+ int i,b[LEW];
+ float a[LEW];
+ for(i=0;i<LEW;i++)
+   scanf("%f%d",&a[i],&b[i]);
+ for(i=0;i<LEW;i++)
+    printf("%lf",pow(a[i],b[i]));
+ return 0;
+}
